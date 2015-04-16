64b78fd1d4aa19d1e13cc0e63669b762251b4072
xiewenqiang <xie_wen_qiang@sina.com>
2015-04-16 12:00:45 +0800

    提交
    MVC设计模式

---
+++ DataModelDemo/Controller/AJViewController.h
@@ -0,0 +1,21 @@
+//
+//  AJViewController.h
+//  DataModelDemo
+//
+//  Created by Jianwen on 13-7-24.
+//  Copyright (c) 2013年 Dark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AJViewFirst.h"
+
+@interface AJViewController : UIViewController
+{
+    //view
+    AJViewFirst *_firstView;
+
+    //data
+    NSArray *_dataArray;
+}
+
+@end
